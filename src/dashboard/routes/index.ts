dabeb599157b6e2b18fb8ee3defbf34e862ffe30
Lukaribou <lucasperroud17@gmail.com>
2020-06-01 23:16:49 +0200

    Carte home

--- src/dashboard/routes/index.ts
+++ src/dashboard/routes/index.ts
@@ -1,10 +1,14 @@
 import { bot } from "../..";
+import { EMOJIS} from "../../utils/structs"
 
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('home', {
-        "bot": bot
+        "bot": bot,
+        "guild": bot.guilds.cache.first(),
+        "memberBot": bot.guilds.cache.first().member(bot.user),
+        "EMOJIS": EMOJIS
     });
 });
 
