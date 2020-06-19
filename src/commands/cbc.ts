eaa26bc926f6301184146c2db35b36e22415f7dc
Lukaribou <lucasperroud17@gmail.com>
2020-06-19 20:50:20 +0200

    Update cbc.ts

--- src/commands/cbc.ts
+++ src/commands/cbc.ts
@@ -7,7 +7,7 @@ export default class CBCCommand extends Command {
     aliases = ['cbc']
     categorie = 'Autre';
 
-    async execute(args: CommandParams): Promise<void> {
+    async execute(args: CommandParams) {
         if (args.message.member.hasPermission("ADMINISTRATOR") && !args.message.mentions.members.first()) { args.message.channel.send(`${EMOJIS.XEMOJI} **Cette commande n'est pas disponible pour une personne possédant les permissions administrateur.**`); return; }
         if (args.message.mentions.members.first() && !args.message.member.hasPermission("ADMINISTRATOR")) { args.message.channel.send(`${EMOJIS.XEMOJI} **Seule une personne avec les permissions administrateur peut effectuer cette commande à la place de quelqu'un.**`); return; }
         args.bot.emit("guildMemberAdd", args.message.mentions.members.first() ? args.message.mentions.members.first() : args.message.member);
