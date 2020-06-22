f2016112441865391651316e9748422bc0a9b9c6
Lukaribou <lucasperroud17@gmail.com>
2020-06-22 23:47:36 +0200

    Update events.ts

--- src/events.ts
+++ src/events.ts
@@ -29,18 +29,27 @@ export async function onGuildMemberJoin(member: GuildMember): Promise<void> { //
      * Renvoie l'émoji à partir de son id
      * @param {string} e id de l'émoji
      */
-    const emf = (e: string): EmojiResolvable => isNaN(parseInt(e)) ? e : bot.emojis.cache.get(e); // Façon de déclarer une fonction sur une ligne | opérateur ternaire =   condition ? true : false
-    // parseInt(): convertit l'argument en nombre, si il ne réussit pas il renvoie NaN
+    const emf = (e: string): EmojiResolvable => isNaN(parseInt(e)) ? e : bot.emojis.cache.get(e);
+    // Façon de déclarer une fonction sur une ligne | opérateur ternaire =   condition ? true : false
 
-    updateStatus()
+    updateStatus();
 
-    let em: MessageEmbed = new MessageEmbed().setColor(0x0000FF).setDescription(`Cliquez sur les réactions pour choisir quels bots vous souhaitez utiliser.\n${EMOJIS.WARNINGEMOJI} Si le salon existe déjà, il est possible qu'il soit dupliqué.\n\n${EMOJIS.OKEMOJI} pour valider, ${EMOJIS.XEMOJI} pour annuler.`); // 0x0000FF = #0000FF = 0, 0, 255 = bleu
-    botsListDb.list.forEach((e: [string, string]) => em.addField(`**__${member.guild.member(e[0]).user.username}__**`, emf(e[1]), true)); // e[0] = id, e[1] = émoji
+    let em: MessageEmbed = new MessageEmbed()
+        .setColor(0x0000FF)
+        .setDescription(`Cliquez sur les réactions pour choisir quels bots vous souhaitez utiliser.\n${EMOJIS.WARNINGEMOJI} Si le salon existe déjà, il est possible qu'il soit dupliqué.\n\n${EMOJIS.OKEMOJI} pour valider, ${EMOJIS.XEMOJI} pour annuler.`); // 0x0000FF = #0000FF = 0, 0, 255 = bleu
+    botsListDb.list
+        .forEach((e: [string, string]) =>
+            em.addField(`**__${member.guild.member(e[0]).user.username}__**`, emf(e[1]), true)); // e[0] = id, e[1] = émoji
+
     member.send(em)
-        .catch(() => (<TextChannel>member.guild.channels.cache.get('705851074921234432')).send(`${EMOJIS.WARNINGEMOJI} ${member} **Je n'arrive pas à vous envoyer un message privé. Changez vos paramètres puis faites la commande \`${bot.config.prefix}cbc\` pour que je vous renvoie le message et que vous puissiez choisir vos salons.**`))
-        .then(async (msg: Message) => { // Une fois que le message a été envoyé, on le récupère
-            const collector: ReactionCollector = msg.createReactionCollector((_, user: User) => user.id == member.user.id, { time: 3e5 }); // On ne prend que les réactions de l'utilisateur | 3e5 ms = 5 minutes
-            botsListDb.list.forEach(async (e: [string, string]) => await msg.react(emf(e[1])).catch()); // Pour chaque entrée on réagit avec l'émoji donné
+        .catch(() => (<TextChannel>member.guild.channels.cache.get('705851074921234432')).send(`${EMOJIS.WARNINGEMOJI} ${member} **Je n'arrive pas à vous envoyer un message privé. Changez vos paramètres puis faites la commande \`${bot.config.prefix}cbc\` pour que je vous renvoie le message et que vous puissiez choisir vos salons.**`)).catch()
+        .then(async (msg: Message) => {
+            const collector: ReactionCollector = msg.createReactionCollector((_, user: User) =>
+                user.id == member.user.id, { time: 3e5 }); // On ne prend que les réactions de l'utilisateur | 3e5 ms = 5 minutes
+
+            botsListDb.list.forEach(async (e: [string, string]) =>
+                await msg.react(emf(e[1])).catch()); // Pour chaque entrée on réagit avec l'émoji donné
+
             await msg.react(EMOJIS.OKEMOJI).catch();
             await msg.react(EMOJIS.XEMOJI).catch();
 
@@ -56,8 +65,10 @@ export async function onGuildMemberJoin(member: GuildMember): Promise<void> { //
                     // rien mais on le met quand même
                 };
             });
+
             collector.on('end', async (collected: Collection<string, MessageReaction>, reason: string) => {
                 let msgContent: string = "";
+
                 switch (reason) {
                     case 'okemoji':
                         if (collected.size == 1) { msgContent = `${EMOJIS.WARNINGEMOJI} **Vous n'avez choisi aucun bot. Si vous souhaitez réavoir ce menu, faites la commande \`cbc\` sur le serveur.**`; break; }
@@ -66,33 +77,38 @@ export async function onGuildMemberJoin(member: GuildMember): Promise<void> { //
                         msgContent.concat(`${EMOJIS.OKEMOJI} **Je vais vous créer les salons pour les bots:**`);
                         collected.delete(EMOJIS.OKEMOJI); // On retire le OK
 
-                        var userCategory: CategoryChannel = <CategoryChannel>member.guild.channels.cache.filter(c => c.type == 'category' && !c.permissionsFor(member.guild.roles.everyone).has("VIEW_CHANNEL") && c.permissionsFor(member).has(["STREAM", "USE_VAD", "PRIORITY_SPEAKER"])).first(); // On filtre les salons en gardant que les catégories
+                        var userCategory: CategoryChannel = <CategoryChannel>member.guild.channels.cache
+                            .filter(c => c.type == 'category'
+                                && !c.permissionsFor(member.guild.roles.everyone).has("VIEW_CHANNEL")
+                                && c.permissionsFor(member).has(["STREAM", "USE_VAD", "PRIORITY_SPEAKER"]))
+                            .first(); // On filtre les salons en gardant que les catégories
 
-                        if (!userCategory) await member.guild.channels.create(member.user.username, { // Si il n'a pas de catégorie on la crée
-                            type: 'category',
-                            reason: `Catégorie inexistante pour création des salons de bots de ${member.user.username}.`,
-                            permissionOverwrites: [
-                                {
-                                    id: member, // Pour l'utilisateur
-                                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'STREAM', 'USE_VAD', 'PRIORITY_SPEAKER']
-                                },
-                                {
-                                    id: member.guild.roles.everyone, // Pour everyone
-                                    deny: ['VIEW_CHANNEL']
-                                },
-                                {
-                                    id: member.guild.roles.cache.get('706783593040445482'), // Pour Muted
-                                    deny: ['SEND_MESSAGES', 'ADD_REACTIONS', 'SPEAK']
-                                }
-                            ]
-                        }).then(c => {
-                            userCategory = c;
-                            member.send(`${EMOJIS.OKEMOJI} **Votre catégorie a été créée. ${EMOJIS.WARNINGEMOJI} Le bot détermine quelle catégorie est la votre grâce aux permissions de celles-ci, merci donc de ne pas les modifier.**`).catch();
-                        }).catch(e => { member.send(`${EMOJIS.XEMOJI} **Une erreur est survenue...\nEnvoyez une capture d'écran de ce message à** ${bot.users.cache.get(bot.config.ownerId)}:\`\`\`${e}\`\`\``).catch(); return; });
+                        if (!userCategory)
+                            await member.guild.channels.create(member.user.username, { // Si il n'a pas de catégorie on la crée
+                                type: 'category',
+                                reason: `Catégorie inexistante pour création des salons de bots de ${member.user.username}.`,
+                                permissionOverwrites: [
+                                    {
+                                        id: member, // Pour l'utilisateur
+                                        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'STREAM', 'USE_VAD', 'PRIORITY_SPEAKER']
+                                    },
+                                    {
+                                        id: member.guild.roles.everyone, // Pour everyone
+                                        deny: ['VIEW_CHANNEL']
+                                    },
+                                    {
+                                        id: member.guild.roles.cache.get('706783593040445482'), // Pour Muted
+                                        deny: ['SEND_MESSAGES', 'ADD_REACTIONS', 'SPEAK']
+                                    }
+                                ]
+                            }).then(c => {
+                                userCategory = c;
+                                member.send(`${EMOJIS.OKEMOJI} **Votre catégorie a été créée. ${EMOJIS.WARNINGEMOJI} Le bot détermine quelle catégorie est la votre grâce aux permissions de celles-ci, merci donc de ne pas les modifier.**`).catch();
+                            }).catch(e => { member.send(`${EMOJIS.XEMOJI} **Une erreur est survenue...\nEnvoyez une capture d'écran de ce message à** ${bot.users.cache.get(bot.config.ownerId)}:\`\`\`${e}\`\`\``).catch(); return; });
 
                         collected.forEach(async (_, key: string) => {
                             var selectedBotId: string = botsListDb.list.find((x: string[]) => x[1] == key)[0]; // On prend le bot correspondant à l'émoji
-                            if (selectedBotId && bot.users.cache.get(selectedBotId)) { // get() renvoie undefined si il n'a rien trouvé, undefined vaut false dans une condition
+                            if (selectedBotId && bot.users.cache.get(selectedBotId)) {
                                 let selectedBot: User = bot.users.cache.get(selectedBotId);
                                 msgContent.concat(` \`${selectedBot.username}\``);
                                 await member.guild.channels.create(selectedBot.username, {
@@ -111,8 +127,9 @@ export async function onGuildMemberJoin(member: GuildMember): Promise<void> { //
                     default:
                         msgContent = `${EMOJIS.WARNINGEMOJI} **Temps imparti de 5 minutes dépassé.**`;
                 };
-                (await (await msg.suppressEmbeds()).reactions.removeAll()).delete()
-                    .then(() => msg.channel.send(msgContent))
+                (await (await msg.suppressEmbeds()).reactions.removeAll())
+                    .delete()
+                    .then(() => msg.channel.send(msgContent).catch())
                     .catch();
             });
         });
@@ -124,14 +141,17 @@ export async function onGuildMemberLeft(member: GuildMember): Promise<void> {
     updateStatus()
     if (member.permissions.any(['ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_ROLES'])) return;
 
-    member.guild.channels.cache.filter(c => c.type === 'category').forEach((c: CategoryChannel) => {
-        if (c.permissionsFor(member)
-            && c.permissionsFor(member).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'STREAM', 'USE_VAD', 'PRIORITY_SPEAKER'])
-            && !c.permissionsFor(member.guild.roles.everyone).has('VIEW_CHANNEL')) {
-            c.children.forEach(child => child.delete(`Suppression automatique des salons de ${member.user.tag}.`).catch());
-            c.delete(`Suppression automatique des salons de ${member.user.tag}.`).catch();
-        };
-    });
+    member.guild.channels.cache
+        .filter(c => c.type === 'category')
+        .forEach((c: CategoryChannel) => {
+            if (c.permissionsFor(member)
+                && c.permissionsFor(member).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'STREAM', 'USE_VAD', 'PRIORITY_SPEAKER'])
+                && !c.permissionsFor(member.guild.roles.everyone).has('VIEW_CHANNEL')) {
+                c.children.forEach(child =>
+                    child.delete(`Suppression automatique des salons de ${member.user.tag}.`).catch());
+                c.delete(`Suppression automatique des salons de ${member.user.tag}.`).catch();
+            };
+        });
 };
 
 /**
