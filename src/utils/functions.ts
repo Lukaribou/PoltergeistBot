eb6dfda8124dc2e3fb93d9c7c505438f7148d15b
Lukaribou <lucasperroud17@gmail.com>
2020-06-24 19:48:08 +0200

    Create getMemberCategory()

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,10 +1,10 @@
 import { writeFile } from "fs";
-import { User } from "discord.js";
+import { User, GuildMember, CategoryChannel, GuildChannel } from "discord.js";
 import { bot } from "..";
 
 /**
  * Sauvegarde la base de données donnée
- * @param {string} dbname Le nom de la base de données (sans le json)
+ * @param dbname Le nom de la base de données (sans le json)
  */
 export function saveDB(dbname: string): void {
     writeFile(`database/${dbname}.json`, JSON.stringify(require(`../../database/${dbname}.json`), null, 4), (err) => {
@@ -14,8 +14,20 @@ export function saveDB(dbname: string): void {
 
 /**
  * Renvoie si un utilisateur est un admin du bot ou non
- * @param {User} user L'utilisateur
+ * @param user L'utilisateur
  */
 export function isBotAdmin(user: User): boolean {
     return (user.id == bot.config.ownerId || bot.config.adminsId.includes(user.id)); // || = ou, Array.includes() est une fonction qui permet de voir si le tableau contient une certaine valeur
 };
+
+/**
+ * Renvoie la catégorie perso de la personne
+ * @param member Le membre
+ */
+export function getMemberCategory(member: GuildMember): CategoryChannel {
+    return member.hasPermission(['ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_ROLES']) ?
+        null : member.guild.channels.cache.filter(c => c.type === 'category'
+            && c.permissionsFor(member).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'STREAM', 'USE_VAD', 'PRIORITY_SPEAKER'])
+            && !c.permissionsFor(member.guild.roles.everyone).has('VIEW_CHANNEL'))
+            .first() as CategoryChannel;
+}
