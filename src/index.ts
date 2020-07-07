240b3c59a69af23cbf278983cd1df5ea9f3ab6d4
Lukaribou <lucasperroud17@gmail.com>
2020-07-07 23:29:42 +0200

    scheduleTasks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { Client, Collection} from "discord.js";
-import { Command, Config } from "./utils/structs";
+import { Client, Collection, GuildMember, CategoryChannel, TextChannel } from "discord.js";
+import { Command, Config, EMOJIS, statsDb } from "./utils/structs";
 import { readdir } from "fs";
 import { onReady, onMessage, onGuildMemberJoin, onGuildMemberLeft, updateStatus, onReactionAdd, onReactionRemove, onInviteCreate } from "./events";
 import { Stats } from "./utils/stats";
+import { daysBetween, getMemberCategory, saveDB } from "./utils/functions";
+import { scheduleJob } from "node-schedule";
 
 export class Poltergeist extends Client { // extends Client = hérite des propriétés et méthodes de Discord.Client
     // bot
@@ -61,3 +63,54 @@ export class Poltergeist extends Client { // extends Client = hérite des propri
 }
 
 export const bot: Poltergeist = new Poltergeist(new Config());
+
+scheduleJob('0 0 0 * * *', () => { // Suppression salons inutilisés
+    bot.guilds.cache.first().members.cache
+        .filter(gm => daysBetween(gm.joinedTimestamp) > 7) // Rejoint +7j
+        .forEach(async (gm: GuildMember) => {
+            var categ: CategoryChannel = getMemberCategory(gm);
+
+            if (categ) {
+                var liste: string[] = [];
+
+                categ.children
+                    .filter((c: TextChannel) => daysBetween(c.createdTimestamp) > 7) // Salons vieux +7j
+                    .forEach(async (c: TextChannel) => {
+                        if ((await c.messages.fetch({ limit: 1 }).catch(() => new Collection)).size === 0) {// => 0 message
+                            c.delete('[Suppression automatique] - Salon inutilisé').catch(() => { });
+                            liste.push(c.name);
+                        }
+                    });
+
+                categ.fetch()
+                    .then((c) => {
+                        if (c.children.size === 0) {
+                            categ.delete('[Suppression automatique] - 0 salon').catch(() => { }); // On supprime aussi la catégorie
+                            gm.user.send(`${EMOJIS.WARNINGEMOJI} [__Message automatique__] - Votre catégorie et ses salons ont été **supprimés** de \`${categ.guild.name}\` car vous ne les avez **jamais utilisés**.`).catch(() => { });
+                            return;
+                        }
+                    })
+                    .catch(() => { })
+
+                if (liste.length !== 0) // Salons supprimés mais pas tous
+                    gm.user.send(`${EMOJIS.WARNINGEMOJI} [__Message automatique__] - Le(s) salon(s) "\`${liste.join('`, `')}\`" a/ont été **supprimé(s)** de votre catégorie sur \`${categ.guild.name}\` car vous ne les avez **jamais utilisés.**`).catch(() => { });
+            }
+        });
+});
+
+scheduleJob('0 0 0 1 * *', () => { // Chaque nouveau mois
+    const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+    const guild = bot.guilds.cache.first();
+    statsDb.monthly.push({
+        monthName: months[new Date().getMonth()],
+        year: new Date().getFullYear(),
+        members: guild.members.cache.filter(m => !m.user.bot).size,
+        messages: 0,
+        channels: guild.channels.cache.size
+    });
+    saveDB('stats');
+});
+
+scheduleJob('*/1 * * * *', () => { // Toutes les minutes
+    saveDB('stats')
+});
